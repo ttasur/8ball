f99d0b4c963c209fbc062ec957dfc874bd29a146
ttasur <141031787+ttasur@users.noreply.github.com>
2024-05-23 10:34:56 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-function getAnswer() {
-    let answers = [
-        "Yes",
-        "No",
-        "Maybe",
-        "Try again later",
-        "Definitely"
-    ];
-    let randomIndex = Math.floor(Math.random() * answers.length);
-    let answer = answers[randomIndex];
-    document.getElementById('answer').innerText = answer;
+const answers = ["Yes", "No", "Looks fishy", "Maybe"];
+
+function shakeBall() {
+    const ball = document.getElementById('ball');
+    const answerElem = document.getElementById('answer');
+
+    // Shake animation
+    ball.style.animation = 'shake 0.5s';
+
+    // After the animation, display the answer
+    setTimeout(() => {
+        const randomIndex = Math.floor(Math.random() * answers.length);
+        const answer = answers[randomIndex];
+        answerElem.innerText = answer;
+        ball.style.animation = '';
+    }, 500);
 }
